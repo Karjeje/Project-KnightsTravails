167cfc724a8b9dbb88e31408331350e47d2dd090
Karjeje <193631637+Karjeje@users.noreply.github.com>
2026-02-08 23:54:30 +0200

    Add a function for calculating possible moves

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log('Node template ready 🚀');
+console.log('Node ready 🚀');
+
+//1.
+function getKnightMoves([x, y]) {
+  const offsets = [
+    [2, 1],
+    [2, -1],
+    [-2, 1],
+    [-2, -1],
+    [1, 2],
+    [1, -2],
+    [-1, 2],
+    [-1, -2],
+  ];
+
+  return offsets
+    .map(([dx, dy]) => [dx + x, dy + y])
+    .filter(([newX, newY]) => newX >= 0 && newX <= 7 && newY >= 0 && newY <= 7);
+}
